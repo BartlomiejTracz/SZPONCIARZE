d6a8bc6219e1d990945230a86d6a631a84ca2638
Bartłomiej <btracz05@gmail.com>
2026-01-22 18:33:41 +0100

    Pierwszy commit - sprawdzenie środowiska

--- add-review.php
+++ add-review.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'config/database.php';
 
 header('Content-Type: application/json');
-
+//Test commita
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         $database = new Database();
